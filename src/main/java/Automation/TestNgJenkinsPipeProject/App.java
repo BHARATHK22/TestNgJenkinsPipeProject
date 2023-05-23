448ed59da1ba3a5adfe9a70a87a688c1ceceb831
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2023-05-23 15:04:14 +0530

    first

---
+++ src/main/java/Automation/TestNgJenkinsPipeProject/App.java
@@ -0,0 +1,16 @@
+package Automation.TestNgJenkinsPipeProject;
+
+import org.openqa.selenium.json.Json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi Bharath Hello World!" );
+        
+    }
+}
---
+++ src/test/java/Automation/TestNgJenkinsPipeProject/Test1.java
@@ -0,0 +1,16 @@
+package Automation.TestNgJenkinsPipeProject;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void smokeTest1() {
+		System.out.println("smokeTest1");
+	}
+	@Test
+	public void smokeTest2() {
+		System.out.println("smokeTest2");
+	}
+
+}
